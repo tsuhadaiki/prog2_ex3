e67f66d18417cee3f9d117d7ffd4df51d8fc5e3e
Daiki Tsuha <e175730@ie.u-ryukyu.ac.jp>
2018-01-09 11:01:38 +0900

    メソッド修正2

--- IdeaProjects/Numeron/src/main/java/Numeron2.java
+++ IdeaProjects/Numeron/src/main/java/Numeron2.java
@@ -3,42 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Numeron2 {
-    public static void main(String args[]){
-        Numeron2 Nume = new Numeron2();
-        Nume.Numeron2();
-    }
-
-    /*これは二人用に直す。具体的には入力部の追加と判定とか*/
     public static void Numeron2() {
-        //変数の初期化。
-        /*必要な変数
-         * タイトル、ルール文、答えの格納される配列、
-         * 入力された数字の格納される配列。
-         */
-        String title = "   　ヌメロン    ";// タイトル
-        String rule = "プレイヤーが指定した3つの数字をあてます。\n"
-                + "数字は0から9の間で決めています。\n"
-                + "3つの数字は同じ数字でははありません。\n"
-                + "入力した数字と位置があっていたら1つ当たってたら1イート、\n"
-                + "数字はあっているが位置が違う場合は1バイトとカウントします。\n"
-                + "全ての数字の位置と値が当たっていれば終了です\n\n";
         int[] answer1 = new int[3];// 答えが入る
         int[] input1 = new int[3];// 入力した答えが入る
         int[] answer2 = new int[3];// 答えが入る
         int[] input2 = new int[3];// 入力した答えが入る
         int countturn1 = 0, countturn2 = 0;
         int eat = 0, bit = 0, exit =0;
-        //タイトルとルールの表示
         BufferedReader br
                 = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(title);
-        System.out.println(rule);
-            /*ここに入力部と入力に同じ数字が使われていないかの判定部、ついでにゲームの判定を突っ込んだら１つにはなる。
-            * そのあとどうにかして３人でやるためにmainメソッド+３つのメソッドに分ける必要がある（mainメソッドが７０行を超えるようであれば２つでいい）*/
         System.out.println("一人目の３桁の数字を１個ずつ入力してください");
         for (int i = 0; i < answer1.length; i++) {
-                //自分より前の要素にかぶるやつがないか確かめる。
-                //あったらもう1回入力作業
             System.out.print((i + 1) + "個目 : ");
             try {
                 answer1[i] = Integer.parseInt(br.readLine());
